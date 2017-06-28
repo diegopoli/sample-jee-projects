f1d587cb7fc1e569e725a953af026cef6f4acc16
Diego Poli <diego.apr@gmail.com>
2017-06-28 00:34:09 -0700

    General improvements.

--- webservices-rest-com-jaxrs-e-jersey/src/test/java/com/dpoli/store/Server.java
+++ webservices-rest-com-jaxrs-e-jersey/src/test/java/com/dpoli/store/Server.java
@@ -10,10 +10,11 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class Server {
 
 	public static void main(String[] args) throws IOException {
-		// http://localhost:8080/shoppingCart
+		// Jersey Web Application
+		// Creates an HTTP server compatible with JAX-WS
+		// Access the URL: http://localhost:8080/shoppingCart
 		ResourceConfig config = new ResourceConfig().packages("com.dpoli.store");
 		URI uri = URI.create("http://localhost:8080/");
-		// Creates an HTTP server compatible with JAX-WS
 		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
 		System.out.println("The server is running...");
 		System.in.read();
