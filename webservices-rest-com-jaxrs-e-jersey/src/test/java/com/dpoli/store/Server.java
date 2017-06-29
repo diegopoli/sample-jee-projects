3a9a6b3a1b40016e44abf58b05e1fb803eaa0d05
Diego Poli <diego.apr@gmail.com>
2017-06-29 00:44:16 -0700

    Code refactoring.

--- webservices-rest-com-jaxrs-e-jersey/src/test/java/com/dpoli/store/Server.java
+++ webservices-rest-com-jaxrs-e-jersey/src/test/java/com/dpoli/store/Server.java
@@ -10,14 +10,19 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class Server {
 
 	public static void main(String[] args) throws IOException {
+		HttpServer server = initServer();
+		System.out.println("The server is running...");
+		System.in.read();
+		server.stop();
+	}
+
+	public static HttpServer initServer() {
 		// Jersey Web Application
 		// Creates an HTTP server compatible with JAX-WS
 		// Access the URL: http://localhost:8080/shoppingCart
 		ResourceConfig config = new ResourceConfig().packages("com.dpoli.store");
 		URI uri = URI.create("http://localhost:8080/");
 		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
-		System.out.println("The server is running...");
-		System.in.read();
-		server.stop();
+		return server;
 	}
 }
--- webservices-rest-com-jaxrs-e-jersey/src/test/java/com/dpoli/store/ShoppingCartTest.java
+++ webservices-rest-com-jaxrs-e-jersey/src/test/java/com/dpoli/store/ShoppingCartTest.java
@@ -4,7 +4,10 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.dpoli.store.model.ShoppingCart;
@@ -12,8 +15,21 @@ import com.thoughtworks.xstream.XStream;
 
 public class ShoppingCartTest {
 
+	private HttpServer server;
+
+	@Before
+	public void startServer() {
+		server = Server.initServer();
+	}
+
+	@After
+	public void stopServer() {
+		server.stop();
+	}
+
 	@Test
 	public void testUriConnection() {
+
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080");
 		String content = target.path("/shoppingCart").request().get(String.class);
