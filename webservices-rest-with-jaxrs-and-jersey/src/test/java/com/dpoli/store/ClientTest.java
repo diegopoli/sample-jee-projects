94ed8c9a0457ddb32a2b6ba2d55097d299a9cf8f
Diego Poli <diego.apr@gmail.com>
2017-07-13 23:54:30 -0700

    Added the LoggingFilter from Jersey API for tracking requests and responses.

--- webservices-rest-with-jaxrs-and-jersey/src/test/java/com/dpoli/store/ClientTest.java
+++ webservices-rest-with-jaxrs-and-jersey/src/test/java/com/dpoli/store/ClientTest.java
@@ -4,15 +4,36 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.filter.LoggingFilter;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ClientTest {
 
+	private HttpServer server;
+	private Client client;
+	private WebTarget target;
+
+	@Before
+	public void startServer() {
+		this.server = Server.initServer();
+		ClientConfig config = new ClientConfig();
+		config.register(new LoggingFilter());
+		this.client = ClientBuilder.newClient(config);
+		this.target = client.target("http://www.mocky.io");
+	}
+
+	@After
+	public void stopServer() {
+		this.server.stop();
+	}
+
 	@Test
 	public void testUriConnection() {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://www.mocky.io");
 		String content = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
 		Assert.assertTrue(content.contains("Videogame 4"));
 	}
